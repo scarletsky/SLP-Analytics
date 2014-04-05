06450914ac2ade25081b368b07aa7531cc54004c
scarletsky <scarletsky1025@gmail.com>
2014-04-05 10:59:13 +0800

    chore: use process.cwd instead of path hard code

--- app/scripts/db.js
+++ app/scripts/db.js
@@ -1,6 +1,7 @@
 module.exports = (function () {
+    var cwd = process.cwd();
     var sqlite3 = require('sqlite3').verbose();
-    var db = new sqlite3.Database('/Users/scarlex/Projects/SLP-Analytics/app/models/data.db');
+    var db = new sqlite3.Database(cwd + '/models/data.db');
 
     return db;
 })();
